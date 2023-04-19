d863802993c743d3fb26633aadb9a2375abdeaa0
zhanwang-sky <zhanwang-sky@users.noreply.github.com>
2023-04-19 22:36:12 +0800

    Initial Commit

---
+++ gray_scale/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  gray_scale
+//
+//  Created by jichen on 2023/4/19.
+//
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
